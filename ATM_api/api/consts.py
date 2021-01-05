c561d9ae7553d348812cc18272af3efa5aac2923
PazBazak <yolobazak1@gmail.com>
2021-01-05 14:47:59 +0200

    updating DB when withdrawing
    When sending a withdraw request, if the ATM passes the request and verifies everything up to the end.
    
    right before sending a resposne to the user, it deletes the needed recrods from the DB.

--- ATM_api/api/consts.py
+++ ATM_api/api/consts.py
@@ -1,4 +1,4 @@
-
+from decimal import Decimal
 
 # region bills
 
@@ -8,6 +8,13 @@ BILL_50 = 'B50'
 BILL_20 = 'B20'
 
 VALID_BILLS = [BILL_200, BILL_100, BILL_50, BILL_20]
+
+BILLS_VALUES = {
+    BILL_200: 200,
+    BILL_100: 100,
+    BILL_50: 50,
+    BILL_20: 20,
+}
 
 # endregion
 
@@ -21,6 +28,14 @@ COIN_001 = 'C0_01'
 
 VALID_COINS = [COIN_10, COIN_5, COIN_1, COIN_01, COIN_001]
 
+COINS_VALUES = {
+    COIN_10: Decimal('10.0'),
+    COIN_5: Decimal('5.0'),
+    COIN_1: Decimal('1.0'),
+    COIN_01: Decimal('0.1'),
+    COIN_001: Decimal('0.01'),
+}
+
 # endregion
 
 # region responses
@@ -29,8 +44,10 @@ ERROR_AMOUNT_RES = {"result": {"error": "ATM does not have enough funds, please 
 
 # endregion
 
-CURRENCY = "currency"
-AMOUNT = "amount"
+CURRENCY = 'currency'
+AMOUNT = 'amount'
+
+PK = 'pk'
 
 VALID_CURRENCIES = ['ILS', 'USD']
 
